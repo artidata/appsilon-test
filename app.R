7399bcbdf5cfebda9af34948f97e74001f1756cf
imaduddin haetami <imaduddin.htm@gmail.com>
2020-11-17 18:13:09 +0700

    adding processed data dependency for vessel type input and and vessel name input.

--- app.R
+++ app.R
@@ -1,39 +1,40 @@
 library(shiny)
 library(shiny.semantic)
 library(leaflet)
+library(readr)
+library(dplyr)
 
-ui <- semanticPage(
-    segment(
-        class = "basic",
-        a(class="ui blue ribbon label", "Leaflet demo"),
-        leafletOutput("map")
-    ),
-    segment(
-        cards(
-            class = "two",
-            card(class = "red",
-                 div(class = "content",
-                     div(class = "header", "Main title card 1"),
-                     div(class = "meta", "Sub title card 1"),
-                     dropdown_input("type", LETTERS, value = "A")
-                 )
-            ),
-            card(class = "blue",
-                 div(class = "content",
-                     div(class = "header", "Main title card 2"),
-                     div(class = "meta", "Sub title card 1"),
-                     dropdown_input("name", LETTERS, value = "A")
-                 )
-            )
-        )
-    )
+ui = semanticPage(
     
-)
+    segment(
+        class = 'basic',
+        a(class='ui blue ribbon label', 'Leaflet demo'),
+        leafletOutput('map')),
+    
+    segment(cards(
+        class = 'two',
+        
+        card(class = 'red',
+             div(class = 'content',
+                 div(class = 'header', 'Main title card 1'),
+                 div(class = 'meta', 'Sub title card 1'),
+                 dropdown_input('type', NULL))),
+        
+        card(class = 'blue',
+             div(class = 'content',
+                 div(class = 'header', 'Main title card 2'),
+                 div(class = 'meta', 'Sub title card 1'),
+                 dropdown_input('name', NULL))))))
 
-server <- function(input, output, session) {
-    output$map <- renderLeaflet({
-        m <- leaflet() %>% addTiles()
-        m <- m %>% setView(21.00, 52.21, zoom = 12)
+server = function(input, output, session) {
+    data = read_csv('data.csv')
+    update_dropdown_input(session,'type',data %>% distinct(ship_type) %>% pull())
+    observeEvent(input$type,update_dropdown_input(session,'name',data %>% filter(ship_type==input$type) %>% distinct(SHIPNAME) %>% pull()))
+    # coords = reactive({data %>% filter(ship_type==input$type&SHIPNAME==input$SHIPNAME)})
+    # observeEvent(coords,print(coords()))
+    output$map = renderLeaflet({
+        m = leaflet() %>% addTiles()
+        m = m %>% setView(21.00, 52.21, zoom = 12)
         m
     })
 }
