07c96f0cfa7d61a4abb4ec77f8bba31fd7cee7f4
imaduddin haetami <imaduddin.htm@gmail.com>
2020-11-18 02:53:54 +0700

    A fully working shiny app based on the requirement.

--- app.R
+++ app.R
@@ -4,39 +4,48 @@ library(leaflet)
 library(readr)
 library(dplyr)
 
-ui = semanticPage(
+ui <- semanticPage(
     
     segment(
         class = 'basic',
-        a(class='ui blue ribbon label', 'Leaflet demo'),
+        a(class='ui blue ribbon label', 'Appsilon Test'),
         leafletOutput('map')),
     
     segment(cards(
         class = 'two',
         
-        card(class = 'red',
+        card(class = 'blue',
              div(class = 'content',
-                 div(class = 'header', 'Main title card 1'),
-                 div(class = 'meta', 'Sub title card 1'),
+                 div(class = 'header', 'Vessel Type:'),
                  dropdown_input('type', NULL))),
         
         card(class = 'blue',
              div(class = 'content',
-                 div(class = 'header', 'Main title card 2'),
-                 div(class = 'meta', 'Sub title card 1'),
+                 div(class = 'header', 'Vessel Name:'),
                  dropdown_input('name', NULL))))))
 
-server = function(input, output, session) {
-    data = read_csv('data.csv')
+server <- function(input, output, session) {
+    
+    data <- read_csv('data.csv')
+    
     update_dropdown_input(session,'type',data %>% distinct(ship_type) %>% pull())
+    
     observeEvent(input$type,update_dropdown_input(session,'name',data %>% filter(ship_type==input$type) %>% distinct(SHIPNAME) %>% pull()))
-    # coords = reactive({data %>% filter(ship_type==input$type&SHIPNAME==input$SHIPNAME)})
-    # observeEvent(coords,print(coords()))
-    output$map = renderLeaflet({
-        m = leaflet() %>% addTiles()
-        m = m %>% setView(21.00, 52.21, zoom = 12)
-        m
-    })
-}
+    
+    coord <- reactive({data %>% filter(ship_type==input$type&SHIPNAME==input$name)})
+    
+    output$map <- renderLeaflet({
+        m = leaflet() %>% 
+            addTiles() %>% 
+            addAwesomeMarkers(coord()$LON,coord()$LAT,label='start',
+                              icon=awesomeIcons('ship','fa','green','black')) %>% 
+            addAwesomeMarkers(coord()$LON_NEXT,coord()$LAT_NEXT,label='end',
+                              icon=awesomeIcons('ship','fa','red','black')) %>% 
+            addPolylines(lng=c(coord()$LON,coord()$LON_NEXT),
+                         lat=c(coord()$LAT,coord()$LAT_NEXT)) %>% 
+            addLabelOnlyMarkers(lng=(coord()$LON + coord()$LON_NEXT)/2,
+                       lat=(coord()$LAT+ coord()$LAT_NEXT)/2,
+                       label=paste0('distance = ',sprintf("%.2f",coord()$distance),' meters'),
+                       labelOptions=labelOptions(noHide=T))})}
 
 shinyApp(ui = ui, server = server)
